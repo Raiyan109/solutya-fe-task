e1e96f9cb4ec41c7209aae5be51ab3695d3e69ef
Raiyan109 <kraiyan109@gmail.com>
2024-01-18 15:04:23 +0600

    uppernav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,46 @@ import { BiSearch } from 'react-icons/bi'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { BsFillPersonFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+import locationImg from '../assets/location.png'
+import emailImg from '../assets/Email Icon.png'
+import phoneImg from '../assets/Ellipse 1.png'
+import fImg from '../assets/Facebook.png'
+import tImg from '../assets/Twitter.png'
+import lImg from '../assets/Linkdin.png'
+import yImg from '../assets/Youtube.png'
+import iIMg from '../assets/instagram.png'
 const Navbar = () => {
     return (
         <div>
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg> */}
-                        <span className="ml-3 text-xl">AirBnb</span>
-                    </a>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">Any where</a>
-                        <a className="mr-5 hover:text-gray-900">Any week</a>
-                        <a className="mr-5 hover:text-gray-900">Add guest</a>
-                        <div><BiSearch /></div>
+            <header className=" bg-[#010F58]">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center gap-3">
+                    <div className='flex items-center gap-2'>
+                        <div>
+                            <img src={locationImg} alt="" className='w-10 h-10' />
+                        </div>
+                        <h3 className='text-xs text-white'>7124 Landmark Tower,New York</h3>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <div>
+                            <img src={emailImg} alt="" className='w-10 h-10' />
+                        </div>
+                        <h3 className='text-xs text-white'>support@yourdomain.com</h3>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <div>
+                            <img src={phoneImg} alt="" className='w-10 h-10' />
+                        </div>
+                        <h3 className='text-xs text-white'>+61383766284</h3>
+                    </div>
+                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center gap-3">
+                        <img src={fImg} alt="" className='w-10 h-10' />
+                        <img src={iIMg} alt="" className='w-10 h-10' />
+                        <img src={tImg} alt="" className='w-10 h-10' />
+                        <img src={lImg} alt="" className='w-10 h-10' />
+                        <img src={yImg} alt="" className='w-10 h-10' />
 
                     </nav>
-                    <div className='flex justify-center items-center gap-2 border-gray-200 shadow-md p-3 rounded-xl'>
-                        <div><GiHamburgerMenu /></div>
-                        <Link to='/login'><BsFillPersonFill /></Link>
-                    </div>
+
                 </div>
             </header>
         </div>
