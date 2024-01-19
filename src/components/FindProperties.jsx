6556b0cac6dc3e2f2885c3722aebfa21a3a00bc4
Raiyan109 <kraiyan109@gmail.com>
2024-01-19 18:22:36 +0600

    find properties image hover effect

--- src/components/FindProperties.jsx
+++ src/components/FindProperties.jsx
@@ -8,7 +8,10 @@ import img2 from '../assets/find-properties-2.png'
 import img3 from '../assets/find-properties-3.png'
 import { IoStar, IoStarHalf } from "react-icons/io5";
 import squrFt from '../assets/Sqft.png'
+import squrFtYellow from '../assets/Sqft-yellow.png'
 import FindPropBtn from './FindPropBtn';
+import hoverIcon1 from '../assets/find-property-hover-1.png'
+import hoverIcon2 from '../assets/find-property-hover-2.png'
 
 const FindProperties = () => {
     return (
@@ -35,30 +38,108 @@ const FindProperties = () => {
                     <FindPropBtn />
                 </div>
                 <SwiperSlide>
-                    <img src={img1} alt="" />
-                    <div className='flex items-center justify-between gap-32'>
-                        <div className='space-y-3 py-2 px-3'>
-                            <h1 className='text-orange-500 font-bold'>Forture Condo Town</h1>
-                            <div className='flex items-center gap-1 text-[#010F58]'>
-                                <IoStar />
-                                <IoStar />
-                                <IoStar />
-                                <IoStar />
-                                <IoStarHalf />
+                    <div className='group relative'>
+                        <img src={img1} alt="" />
+                        <div className='bg-orange-600 absolute top-32 left-40 py-1 px-3 font-semibold text-white group-hover:hidden'>
+                            Bangkok
+                        </div>
+                        <div className="middle">
+                            <div className="text flex flex-col gap-3">
+                                <div className='flex items-center gap-3'>
+                                    <img src={hoverIcon1} alt="" />
+                                    <img src={hoverIcon2} alt="" />
+                                </div>
+                                <h1 className='text-xl text-white'>John Doe</h1>
+
                             </div>
                         </div>
-                        <div>
-                            <img src={squrFt} alt="" className='sqft' />
+                        <div className='flex items-center justify-between gap-32 group-hover:border-b-2 group-hover:border-orange-600'>
+                            <div className='space-y-3 py-2 px-3'>
+                                <h1 className='text-orange-500 font-bold'>Forture Condo Town</h1>
+                                <div className='flex items-center gap-1 text-[#010F58] text-sm font-medium'>
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStarHalf />
+                                    <p>4.5</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src={squrFt} alt="" className='sqft' />
+                            </div>
                         </div>
                     </div>
 
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={img2} alt="" />
+                    <div className='group relative'>
+                        <img src={img1} alt="" />
+                        <div className='bg-orange-600 absolute top-32 left-40 py-1 px-3 font-semibold text-white group-hover:hidden'>
+                            Sydney
+                        </div>
+                        <div className="middle">
+                            <div className="text flex flex-col gap-3">
+                                <div className='flex items-center gap-3'>
+                                    <img src={hoverIcon1} alt="" />
+                                    <img src={hoverIcon2} alt="" />
+                                </div>
+                                <h1 className='text-xl text-white'>John Doe</h1>
+
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-between gap-32 group-hover:border-b-2 group-hover:border-orange-600'>
+                            <div className='space-y-3 py-2 px-3'>
+                                <h1 className='text-orange-500 font-bold'>Farmhouse for Buy</h1>
+                                <div className='flex items-center gap-1 text-orange-600 text-sm font-medium'>
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStarHalf />
+                                    <p>4.5</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src={squrFtYellow} alt="" className='sqft' />
+                            </div>
+                        </div>
+                    </div>
 
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={img3} alt="" />
+                    <div className='group relative'>
+                        <img src={img1} alt="" />
+                        <div className='bg-orange-600 absolute top-32 left-40 py-1 px-3 font-semibold text-white group-hover:hidden'>
+                            Toronto
+                        </div>
+                        <div className="middle">
+                            <div className="text flex flex-col gap-3">
+                                <div className='flex items-center gap-3'>
+                                    <img src={hoverIcon1} alt="" />
+                                    <img src={hoverIcon2} alt="" />
+                                </div>
+                                <h1 className='text-xl text-white'>John Doe</h1>
+
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-between gap-32 group-hover:border-b-2 group-hover:border-orange-600'>
+                            <div className='space-y-3 py-2 px-3'>
+                                <h1 className='text-orange-500 font-bold'>Apartment for sale</h1>
+                                <div className='flex items-center gap-1 text-orange-600 text-sm font-medium'>
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStar />
+                                    <IoStarHalf />
+                                    <p>4.5</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src={squrFtYellow} alt="" className='sqft' />
+                            </div>
+                        </div>
+                    </div>
 
                 </SwiperSlide>
 
