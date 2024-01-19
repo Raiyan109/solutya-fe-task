41bc4894e1651ccb7a82d04299699558ee88e9e5
Raiyan109 <kraiyan109@gmail.com>
2024-01-19 09:31:08 +0600

    hero bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,20 @@
-import heroImg from '../assets/Hero Section.png'
+import heroImg from '../assets/hero-right-img.png'
+import blue from '../assets/hero-left-blue.png'
+import orange from '../assets/hero-left-orange.png'
 
 const Hero = () => {
     return (
-        <div>
-            <img src={heroImg} alt="" />
+        <div className='flex items-center justify-between bg-navy-blue'>
+            <div className='flex-1 relative'>
+
+                <img src={orange} alt="" className='absolute -top-[400px] left-44 z-0 h-[800px] w-[1200px]' />
+                <img src={blue} alt="" className='absolute -top-[400px] left-40 z-20 h-[800px] w-[1200px]' />
+            </div>
+            <div className='flex-3'>
+                <img src={heroImg} alt=""
+                // className='w-[800px] h-[800px] object-contain' 
+                />
+            </div>
         </div>
     );
 };
